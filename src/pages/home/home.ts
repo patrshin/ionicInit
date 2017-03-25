3dda13c131b4c537690f2c7c639842f7b60f8c4c
Patrick Shin <pmjshin@gmail.com>
2017-03-24 23:59:45 -0400

    Kinda working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  name1 = '';
+  name2 = '';
+
+  get score() {
+    var i = 0;
+    var score1 = 0;
+    var j = 0;
+    var score2 = 0;
+    for (i = 0; i < this.name1.length; i++) {
+      score1 += this.name1.charCodeAt(i) - 50;
+    }
+    score1 /= this.name1.length
+
+    for (j = 0; j < this.name2.length; j++) {
+      score2 += this.name2.charCodeAt(j) - 50;
+    }
+    score2 /= this.name2.length
+
+    return (score2 > score1 ? score1/score2*100 : score2/score1*100);
 
   }
-
 }
